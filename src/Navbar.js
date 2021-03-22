581e7ab64f26d28e9a8040cdf49759e7c9274aa8
Vyshnav-KS <Vyshnavks85@gmail.com>
2021-03-22 21:34:47 +0530

    navbar bug

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,16 +5,16 @@ const Navbar = () => {
         <nav className="navbar">
         <div className= "links">
         <input type="checkbox" id= "check"/>
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
+        <label for="check" className="checkbtn">
+            <i className="fas fa-bars"></i>
         </label>
-        <label class="nav_logo"><img class="nav_logo" src="" alt="Image"/></label>
+        <label className="nav_logo"><img className="nav_logo" src="" alt="Image"/></label>
         <ul>
             <li><a href="">Home</a></li>
             <li><a href="">Blog</a></li>
             <li><a href="">Services</a></li>
             <li><a href="">Contact</a></li>
-            <li><a href="" class="nav_active">Join Now</a></li>
+            <li><a href="" className="nav_active">Join Now</a></li>
         </ul>
         </div>
     </nav>
