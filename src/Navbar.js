570ce42ca0042dc940a0493cbf7637fdd5a856d4
Vyshnav-KS <Vyshnavks85@gmail.com>
2021-03-22 21:31:38 +0530

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,21 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Hack Club cucek</h1>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/blog">Blog</Link>
-                <Link to="/join">Join</Link>
-            </div>
-        </nav>
+        <div className= "links">
+        <input type="checkbox" id= "check"/>
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="nav_logo"><img class="nav_logo" src="" alt="Image"/></label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="" class="nav_active">Join Now</a></li>
+        </ul>
+        </div>
+    </nav>
     );
 }
 
