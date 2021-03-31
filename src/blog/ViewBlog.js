148f0fd88116d246c5e8ffb0ef915a649a6df876
superRaptor911 <raptor.inc2018@gmail.com>
2021-03-31 21:30:46 +0530

    View post author name and date

--- src/blog/ViewBlog.js
+++ src/blog/ViewBlog.js
@@ -2,12 +2,14 @@ import {useState} from "react";
 import {useParams} from "react-router";
 import {Error_showError, Msg_Loading} from "../Messages";
 import useFetch from "../useFetch";
-import {serverAddress} from "../Utility";
+import {getCookie, serverAddress} from "../Utility";
 import RenderPost from "./RenderPost";
 import { makeStyles } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography';
 import UserAvatar from "../components/UserAvatar";
 import Container from '@material-ui/core/Container'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) =>({
   root: {
@@ -32,12 +34,22 @@ const useStyles = makeStyles((theme) =>({
   },
   iconText: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 10
+    justifyContent: 'left',
+    marginLeft: 10,
+    paddingTop: 5
+  },
+  optionBtn: {
+    marginLeft: 'auto',
   }
 }));
 
+const showOptionBtn = (author, style) => {
+  if (getCookie("username") === author) {
+    // return (
+    // );
+  }
+  return "";
+}
 
 const ViewBlog = () => {
   const classes = useStyles()
@@ -50,6 +62,7 @@ const ViewBlog = () => {
   let previewImg="";
   let content="";
   let author="";
+  let date="";
   console.log(target.uri);
 
   if (serverResponse.isLoading) {
@@ -65,8 +78,11 @@ const ViewBlog = () => {
     title = serverResponse.data.post.title;
     previewImg = serverResponse.data.post.preview;
     author = serverResponse.data.post.author;
+    date = serverResponse.data.post.date;
     content = (<div dangerouslySetInnerHTML={{ __html: serverResponse.data.post.content}}/>)
   }
+
+
   return (
     <div className={classes.root}>
       <Typography variant="h2" className={classes.title}>
@@ -74,7 +90,13 @@ const ViewBlog = () => {
       </Typography>
       <Container className={classes.avatarContainer}>
         {author && (<UserAvatar username={author} className={classes.largeIcon}/>)}
+        <div>
           <Typography variant="h6" className={classes.iconText}>{author}</Typography>
+          <Typography variant="h7" className={classes.iconText}>{date}</Typography>
+        </div>
+      <IconButton className={classes.optionBtn}>
+        <MoreVertIcon/>
+      </IconButton>
       </Container>
       <br/><br/>
       <RenderPost
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,43 +1,45 @@
 import {useState, useEffect} from 'react';
 
 const useFetch = (target) => {
-    const [data, setData] = useState(null);
-    const [isLoading, setLoading] = useState(true);
-    const [error, setError] = useState({error: false, msg: ""});
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState({error: false, msg: ""});
 
-    useEffect(() =>{
-        const abortCont = new AbortController();
-        fetch(target.uri, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(target.data),
-                signal: abortCont.signal
-            })
-            .then(res => {
-                if (!res.ok) {
-                    console.log("useFetch::could not fetch data");
-                    abortCont.abort();
-                    throw Error("Could not fetch data");
-                }
-                return res.json();
-            })
-            .then(data => {
-                setData(data);
-                setLoading(false);
-                setError({error: false, msg:""});
-            })
-            .catch(err => {
-                if (err.name === 'AbortError') {
-                    console.log("Aborting.....");
-                    setError({error: true, msg: err.message});
-                } else {
-                    setError({error: true, msg: err.message});
-                    setLoading(false);
-                }
-            });
-        return () => abortCont.abort();
-    }, [target]);
-    return {data, isLoading, error};
+  useEffect(() =>{
+    const abortCont = new AbortController();
+    if (target.uri !== "") {
+      fetch(target.uri, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(target.data),
+        signal: abortCont.signal
+      })
+        .then(res => {
+          if (!res.ok) {
+            console.log("useFetch::could not fetch data");
+            abortCont.abort();
+            throw Error("Could not fetch data");
+          }
+          return res.json();
+        })
+        .then(data => {
+          setData(data);
+          setLoading(false);
+          setError({error: false, msg:""});
+        })
+        .catch(err => {
+          if (err.name === 'AbortError') {
+            console.log("Aborting.....");
+            setError({error: true, msg: err.message});
+          } else {
+            setError({error: true, msg: err.message});
+            setLoading(false);
+          }
+        });
+    }
+    return () => abortCont.abort();
+  }, [target]);
+  return {data, isLoading, error};
 }
 
 export default useFetch;
