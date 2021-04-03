61360fc1de36d682dd3c49cbabe440b647f01334
superRaptor911 <raptor.inc2018@gmail.com>
2021-04-03 12:27:02 +0530

    Sortby option in post list, modified build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@ server1="34.67.194.167"
 
 echo "Generating Files"
 rm -rf build/
-yarn build && wget https://github.com/Teams-cs/cucekhackclubServer/archive/refs/heads/main.zip &&
-    unzip main.zip && rm main.zip && mv cucekhackclubServer-main build/ &&
-    mv build/cucekhackclubServer-main build/server && cp .htaccess build/
+yarn build && 
+wget https://github.com/Teams-cs/cucekhackclubServer/archive/refs/heads/main.zip &&
+unzip main.zip && rm main.zip &&
+mv cucekhackclubServer-main build/ &&
+mv build/cucekhackclubServer-main build/server && 
+cp .htaccess build/ &&
+echo "Site generated in build/"
 
 echo "Do you want to upload generated files into server? [y/n]"
 read choice
